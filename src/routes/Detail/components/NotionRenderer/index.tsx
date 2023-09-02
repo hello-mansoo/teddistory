a0122bf2d3dfc3daf3d9eb8a32017f6daf13175f
TwoShot22 <mansoo0621@naver.com>
2023-09-02 23:25:58 +0900

    fix: PostDetail 렌더링 시 Post 내용을 SSR로 처리한다

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -18,7 +18,7 @@ import styled from "@emotion/styled"
 
 const _NotionRenderer = dynamic(
   () => import("react-notion-x").then((m) => m.NotionRenderer),
-  { ssr: false }
+  { ssr: true }
 )
 
 const Code = dynamic(() =>
